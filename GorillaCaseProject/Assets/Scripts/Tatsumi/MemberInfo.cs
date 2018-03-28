829c1182be8a8c72e630c5bff643df9c0bb41937
K <ki3ttm@gmail.com>
2018-03-29 00:17:57 +0900

    フォルダ構成 MemberInfo追加

---
+++ GorillaCaseProject/Assets/Scripts/Tatsumi/MemberInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemberInfo : MonoBehaviour {
+	enum Member {
+		NoSelect,
+		Public,
+		Tatsumi,
+		Tamura,
+		Saito,
+		Murata,
+	}
+
+	[SerializeField] Member member;	// 担当者
+	[SerializeField] string cmnt;   // 備考
+
+	// Use this for initialization
+	void Start() {
+		if (member == Member.NoSelect) {
+			Debug.LogWarning("担当者が設定されていません。\n" + name);
+		}
+	}
+
+	// Update is called once per frame
+//	void Update() {}
+}
