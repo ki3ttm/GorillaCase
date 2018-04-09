79780491fa9c0e376d36d9e07fba9d961e50f01a
SaitoYoshiki <saitosaito888888@gmail.com>
2018-04-09 19:19:22 +0900

    現在の状況

---
+++ GorillaCaseProject/Assets/Scenes/Saito/CollisionTest/SaitoTest_BlockAccel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaitoTest_BlockAccel : MonoBehaviour {
+
+	[SerializeField]
+	Vector3 acceleration = new Vector3(0.0f, -9.8f, 0.0f);
+
+	[SerializeField]
+	bool isReverse = false;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void FixedUpdate() {
+
+		Vector3 lAccel;
+		if(isReverse == false) {
+			lAccel = acceleration;
+		}
+		else {
+			lAccel = -acceleration;
+		}
+
+		GetComponent<Rigidbody>().AddForce(lAccel, ForceMode.Acceleration);
+	}
+}
