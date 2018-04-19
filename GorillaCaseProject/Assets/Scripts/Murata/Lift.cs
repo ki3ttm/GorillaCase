5bbccfead301dfa809e6cffc5b2f83a51d8e5a64
Murata <marke0818@gmail.com>
2018-04-19 19:15:49 +0900

    持ち上げ（仮）

--- GorillaCaseProject/Assets/Scripts/Murata/Lift.cs
+++ GorillaCaseProject/Assets/Scripts/Murata/Lift.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Lift : MonoBehaviour {
     [SerializeField] float maxDis = 10.0f;
+    [SerializeField] GameObject liftObj = null;
+    [SerializeField] bool LR = false;
 
 	// Use this for initialization
 	void Start () {
@@ -12,15 +14,76 @@ public class Lift : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if(LR == false)
+            {
+                LR = true;
+            }
+            else
+            {
+                LR = false;
+            }
+        }
+
+
+        // 持ち上げ下げ
+        LiftUpDown();
+
+        // 持ち続け
+        Lifting();
+	}
+
+    GameObject LiftUp()
+    {
+        Debug.Log("LS");
         RaycastHit hitInfo;
         Debug.DrawRay(transform.position, transform.forward);
         Physics.Raycast(transform.position, transform.forward, out hitInfo, maxDis);
 
-        if(hitInfo.collider.name == "WeightBox")
-        {
+        liftObj = hitInfo.collider.gameObject;
+        return liftObj;
+    }
 
+    void Lifting()
+    {
+        Debug.Log("a");
+        if(liftObj != null)
+        {
+            Debug.Log("b");
+            liftObj.transform.position = transform.position + new Vector3(0, 1.2f, 0);
+        }
+    }
+
+    void LiftDown()
+    {
+        // 降ろす
+        if(LR == false)
+        {
+            // 左
+            liftObj.transform.position = transform.position + new Vector3(1.0f, 0, 0);
+        }
+        else
+        {
+            // 右
+            liftObj.transform.position = transform.position + new Vector3(-1.0f, 0, 0);
         }
 
-	}
+        // もう持ってないから捨てる
+        liftObj = null;
+    }
+
+    void LiftUpDown()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if(liftObj == null)
+            {
+                LiftUp();
+            }else
+            {
+                LiftDown();
+            }
+        }
+    }
 }
