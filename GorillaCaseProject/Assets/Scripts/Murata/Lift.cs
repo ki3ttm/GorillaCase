9f024d78085f8e717a58bd0a6098c0f62deeb265
Murata <marke0818@gmail.com>
2018-04-20 23:32:47 +0900

    持ち上げ処理

--- GorillaCaseProject/Assets/Scripts/Murata/Lift.cs
+++ GorillaCaseProject/Assets/Scripts/Murata/Lift.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Lift : MonoBehaviour {
-    [SerializeField] float maxDis = 10.0f;
+    [SerializeField] float maxDis = 2.0f;
     [SerializeField] GameObject liftObj = null;
-    [SerializeField] bool LR = false;
+
+    // test用
+    [SerializeField] float LR = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -14,18 +16,13 @@ public class Lift : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.Tab))
-        {
-            if(LR == false)
-            {
-                LR = true;
-            }
-            else
-            {
-                LR = false;
-            }
-        }
 
+        Debug.DrawRay(transform.position, transform.forward * maxDis);
+
+        //  Debug.Log("LiftUpdate");
+
+        // 左右
+        LR = Vector3.Dot(transform.forward, Vector3.right);
 
         // 持ち上げ下げ
         LiftUpDown();
@@ -33,57 +30,69 @@ public class Lift : MonoBehaviour {
         // 持ち続け
         Lifting();
 	}
-
-    GameObject LiftUp()
-    {
-        Debug.Log("LS");
-        RaycastHit hitInfo;
-        Debug.DrawRay(transform.position, transform.forward);
-        Physics.Raycast(transform.position, transform.forward, out hitInfo, maxDis);
-
-        liftObj = hitInfo.collider.gameObject;
-        return liftObj;
-    }
-
-    void Lifting()
-    {
-        Debug.Log("a");
-        if(liftObj != null)
-        {
-            Debug.Log("b");
-            liftObj.transform.position = transform.position + new Vector3(0, 1.2f, 0);
-        }
-    }
-
-    void LiftDown()
-    {
-        // 降ろす
-        if(LR == false)
-        {
-            // 左
-            liftObj.transform.position = transform.position + new Vector3(1.0f, 0, 0);
-        }
-        else
-        {
-            // 右
-            liftObj.transform.position = transform.position + new Vector3(-1.0f, 0, 0);
-        }
-
-        // もう持ってないから捨てる
-        liftObj = null;
-    }
-
     void LiftUpDown()
     {
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            if(liftObj == null)
+            Debug.Log("LSDOWN");
+
+            if (liftObj == null)
             {
                 LiftUp();
-            }else
+            }
+            else
             {
                 LiftDown();
             }
         }
     }
+
+    GameObject LiftUp()
+    {
+        Debug.Log("LU");
+        RaycastHit hitInfo;
+        Physics.Raycast(transform.position, transform.forward, out hitInfo, maxDis);
+
+        // ブロックがある
+        if (hitInfo.collider != null)
+        {
+            liftObj = hitInfo.collider.gameObject;
+            // 持ち上げた箱の当たり判定を消す
+            BoxCollider col = liftObj.GetComponent<BoxCollider>();
+            if (col != null)
+            {
+                col.enabled = false;
+            }
+            return liftObj;
+        }
+        return null;
+    }
+
+    void LiftDown()
+    {
+        Debug.Log("LD");
+
+        // 降ろす
+        liftObj.transform.position = transform.position + new Vector3(LR, 0, 0);
+
+        // 持ち上げた箱の当たり判定を戻す
+        BoxCollider col = liftObj.GetComponent<BoxCollider>();
+        if (col != null)
+        {
+            col.enabled = true;
+        }
+        // もう持ってないから捨てる
+        liftObj = null;
+    }
+
+    void Lifting()
+    {
+       // Debug.Log("a");
+        if(liftObj != null)
+        {
+          //  Debug.Log("b");
+            liftObj.transform.position = transform.position + new Vector3(0, 1.2f, 0);
+        }
+    }
+
 }
